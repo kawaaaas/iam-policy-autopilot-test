39481cf0dd13aeffde595ca1b9825aee1f9773ef
kawaaaas <sho1314_2423@yahoo.co.jp>
2025-12-23 12:11:52 +0900

    feat: EventBridge コンストラクトを追加
    - カスタムイベントバスの作成機能
    - イベント送信権限付与メソッドの実装

---
+++ lib/constructs/eventbridge-construct.ts
@@ -0,0 +1,62 @@
+import * as events from "aws-cdk-lib/aws-events";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+/**
+ * EventBridge コンストラクトのプロパティ
+ */
+export interface EventBridgeConstructProps {
+  /**
+   * イベントバス名
+   */
+  readonly eventBusName: string;
+
+  /**
+   * イベントバスの説明（オプション）
+   */
+  readonly description?: string;
+}
+
+/**
+ * EventBridge コンストラクト
+ *
+ * カスタム EventBridge イベントバスを作成し、設定を管理する再利用可能なコンストラクトです。
+ * デフォルトのイベントバスとは独立して動作します。
+ */
+export class EventBridgeConstruct extends Construct {
+  /**
+   * 作成された EventBridge イベントバス
+   */
+  public readonly eventBus: events.EventBus;
+
+  /**
+   * イベントバスの ARN
+   */
+  public readonly eventBusArn: string;
+
+  /**
+   * イベントバス名
+   */
+  public readonly eventBusName: string;
+
+  constructor(scope: Construct, id: string, props: EventBridgeConstructProps) {
+    super(scope, id);
+
+    // カスタムイベントバスの作成
+    this.eventBus = new events.EventBus(this, "EventBus", {
+      eventBusName: props.eventBusName,
+      description: props.description,
+    });
+
+    // プロパティの設定
+    this.eventBusArn = this.eventBus.eventBusArn;
+    this.eventBusName = this.eventBus.eventBusName;
+  }
+
+  /**
+   * 指定されたプリンシパルにイベント送信権限を付与
+   */
+  public grantPutEventsTo(grantee: iam.IGrantable): iam.Grant {
+    return this.eventBus.grantPutEventsTo(grantee);
+  }
+}
